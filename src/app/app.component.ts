95f93c85ce16c42ce60c391b21c7b60c77731e92
HaiLe81 <lekhachai9999@gmail.com>
2020-08-01 15:21:06 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado', 'Hai Le'];
+  user = {
+    name: 'Hai Le',
+    age: 22
+  }
+  handler(event){
+    console.log('Clicked', event.target.value)
+  }
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -7,4 +7,12 @@ import { Component, Input } from '@angular/core';
 })
 export class HelloComponent  {
   @Input() name: string;
+
+  ngOnInit(){
+    console.log('Init')
+  }
+
+  ngOnDestroy(){
+    console.log('Destroy')
+  }
 }
