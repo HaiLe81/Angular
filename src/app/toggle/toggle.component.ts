f9a4687c99875c84589bd1778475c1a8251fffff
HaiLe81 <lekhachai9999@gmail.com>
2020-08-03 00:39:08 +0700

    3 files modified, 4 added

---
+++ src/app/toggle/toggle.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: 'toggle',
+    templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.css']
+})
+export class ToggleComponent {
+  @Input() checked = false;
+  @Output() checkedChange = new EventEmitter<boolean>();
+}
