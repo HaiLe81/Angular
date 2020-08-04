9ad653faf2fd8564121215515e3fde55822bbd18
HaiLe81 <lekhachai9999@gmail.com>
2020-08-05 00:21:02 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, ViewChild, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { ToggleComponent } from './toggle/toggle.component';
 
 @Component({
   selector: 'my-app',
@@ -6,6 +7,19 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  @ViewChildren(ToggleComponent) toggleComps: QueryList<ToggleComponent>;
+  // @ViewChild('toggleComp', {static: true}) toggleComp: ToggleComponent;
+  @ViewChild('toggleBtn', {static: true}) toggleBtn: ElementRef<HTMLButtonElement>;
+  @ViewChild('nameInput', {static: true}) nameInput: ElementRef<HTMLInputElement>;
+
   name = 'Angular ' + VERSION.major;
   isChecked = true;
+  showlast = true;
+  ngOnInit(){
+    this.nameInput.nativeElement.focus();
+    console.log('init', this.nameInput);
+  }
+  ngAfterViewInit(){
+    this.toggleComps.changes.subscribe(console.log);
+  }
 }
